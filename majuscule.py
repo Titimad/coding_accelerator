4f9e70f0f410c29d85bf7d17930b8cd54f85b069
Titimad <84244859+Titimad@users.noreply.github.com>
2021-05-18 12:25:35 +0200

    Add files via upload

---
+++ majuscule.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+import sys
+
+string = sys.argv[1]
+result = ""
+rang_lettre = 1
+string = string.lower()
+for i in range(len(string)):
+    
+    if string[i].isalpha() and rang_lettre == 0:
+            result = result + string[i].upper()
+            rang_lettre = 1
+    elif string[i].isalpha() and rang_lettre == 1:
+            result = result + string[i]
+            rang_lettre =0
+    else:
+        result = result + string[i]
+
+print (result)
+        
